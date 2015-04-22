0ce6e242577b502b4b95b04f28ee43ae70d9eec6
jeanice1 <jeanice1@umbc.edu>
2015-04-22 01:38:42 -0400

    Re/Scheduling: Added unfinished functionality

--- StudProcessHome.php
+++ StudProcessHome.php
@@ -8,6 +8,7 @@ elseif($_POST["selection"] == 'View'){
 	header('Location: 04StudViewApp.php');
 }
 elseif($_POST["selection"] == 'Reschedule'){
+	$_SESSION["resch"] = true;
 	header('Location: 03StudSelectType.php');
 }
 elseif($_POST["selection"] == 'Cancel'){
---
+++ StudProcessSch.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+//if stud DNE set student? here? {}
+//if postFINISH = submit {
+//if stud DNE set student? or here?
+//set everything: stud info & update}
+//header:Home
+?>
